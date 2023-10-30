4f9009e580cf76c1c6adf6767fbd4061951bf239
Ilya Fomin <7foma7@mail.ru>
2023-10-30 20:11:38 +0300

    add sort_date

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,25 @@
-def check_dict(data: list[dict], state: str = "EXECUTED") -> list[dict]:
-    res = []
-    for d in data:
-        if d["state"] == state:
-            res.append(d)
-    return res
+from typing import Any
 
 
+def check_dict(data: list[Any], state: str = "EXECUTED") -> list[Any]:
+    """
+    Функия возвращает список словарей с определеным ключем
+    :param data: list
+    :param state: str
+    :return: list
+    """
+    result = []
+    for diction in data:
+        if diction["state"] == state:
+            result.append(diction)
+    return result
+
+
+def sort_date(data: list[Any], sort: bool = True) -> list[Any]:
+    """
+    Функия возвращает отсортированый по дате список словарей
+    :param data: list
+    :param sort: list
+    :return: list
+    """
+    return sorted(data, key=lambda x: x["date"], reverse=sort)

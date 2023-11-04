7afc0a169a4b2f2de51632c96c44438e09aa70f1
Ilya Fomin <7foma7@mail.ru>
2023-11-04 17:18:59 +0300

    fix mypy, black in test_processing.py and test_masks.py.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,18 +1,27 @@
-from src.masks import number_card, account_number
+from typing import Any
+
 import pytest
 
+from src.masks import account_number, number_card
 
-@pytest.mark.parametrize("number, expected_result", [("7000792289606361", "7000 79** **** 6361"),
-                                                     ("1596837868705199", "1596 83** **** 5199"),
-                                                     ("6831982476737658", "6831 98** **** 7658"),
-                                                     ("7158300734726758", "7158 30** **** 6758"),
-                                                     ("8990922113665229", "8990 92** **** 5229")])
-def test_number_card(number, expected_result):
+
+@pytest.mark.parametrize(
+    "number, expected_result",
+    [
+        ("7000792289606361", "7000 79** **** 6361"),
+        ("1596837868705199", "1596 83** **** 5199"),
+        ("6831982476737658", "6831 98** **** 7658"),
+        ("7158300734726758", "7158 30** **** 6758"),
+        ("8990922113665229", "8990 92** **** 5229"),
+    ],
+)
+def test_number_card(number: str, expected_result: str) -> Any:
     assert number_card(number) == expected_result
 
 
-@pytest.mark.parametrize("account, expected_result", [("73654108430135874305", "**4305"),
-                                                      ("35383033474447895560", "**5560"),
-                                                      ("64686473678894779589", "**9589")])
-def test_account_number(account, expected_result):
+@pytest.mark.parametrize(
+    "account, expected_result",
+    [("73654108430135874305", "**4305"), ("35383033474447895560", "**5560"), ("64686473678894779589", "**9589")],
+)
+def test_account_number(account: str, expected_result: str) -> Any:
     assert account_number(account) == expected_result

3daa5f1e8f7bea31c4535b3994cd1446a568bd30
Ilya Fomin <7foma7@mail.ru>
2023-11-08 17:42:24 +0300

    homework1.1

--- tests/test_add_task.py
+++ tests/test_add_task.py
@@ -5,10 +5,10 @@ import pytest
 from src.add_task import count_number_in_list
 
 
-# @pytest.fixture
-# def my_num_list() -> tuple[list[Any], int]:
-#     return [1, 2, 5, 5, 6, 5, 3, 6], 5
-#
-#
-# def test_count_number_in_list(my_num_list: tuple[list[Any], int]) -> Any:
-#     assert count_number_in_list(my_num_list[0], my_num_list[1]) == 3
+@pytest.fixture
+def my_num_list() -> tuple[list[Any], int]:
+    return [1, 2, 5, 5, 6, 5, 3, 6], 5
+
+
+def test_count_number_in_list(my_num_list: tuple[list[Any], int]) -> Any:
+    assert count_number_in_list(my_num_list[0], my_num_list[1]) == 3
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,27 +1,27 @@
-# from typing import Any
-#
-# import pytest
-#
-# from src.processing import check_dict, sort_date
-#
-#
-# @pytest.fixture
-# def my_data() -> list[dict[str, object]]:
-#     """Создание фикстуры"""
-#     return [
-#         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-#         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-#         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-#         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-#     ]
-#
-#
-# def test_check_dict(my_data: list[dict[str, object]]) -> Any:
-#     """тест для провекри функции check_dict"""
-#     assert check_dict(my_data) == [
-#         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-#         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-#     ]
+from typing import Any
+
+import pytest
+
+from src.processing import check_dict, sort_date
+
+
+@pytest.fixture
+def my_data() -> list[dict[str, object]]:
+    """Создание фикстуры"""
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+def test_check_dict(my_data: list[dict[str, object]]) -> Any:
+    """тест для провекри функции check_dict"""
+    assert check_dict(my_data) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
 
 
 def test_sort_data(my_data: list[dict[str, object]]) -> Any:

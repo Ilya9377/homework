df9b7e4d5e1bbde56505dd272dbeaf267bdf0399
Ilya Fomin <7foma7@mail.ru>
2023-11-14 22:35:33 +0300

    fix homework

--- src/decorators.py
+++ src/decorators.py
@@ -1,29 +1,39 @@
-import datetime
-from typing import Any, Callable
+from datetime import datetime
+from functools import wraps
+from typing import Any, Callable, Optional
 
 
-def log(filename: str = "error.txt") -> Callable[..., Any]:
+def log(filename: Optional[str] = None) -> Callable[..., Any]:
     """Декоратор для логирования ошибок выполнения функции."""
+
     def my_decor(function: Callable[..., int]) -> Any:
         """Внутренний декоратор для реализации логирования."""
-        def inner(*args: tuple[int], **kwargs: dict[int, int]) -> float:
+
+        @wraps(function)
+        def inner(*args: tuple[int], **kwargs: dict[int, int]) -> Any:
             """Внутренняя функция, выполняющая логирование и вызов целевой функции."""
-            with open(filename, "a") as f:
-                data = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                try:
-                    result = function(args, kwargs)
-                    f.write(f"{data} {function.__name__} ok\n")
-                    return result
-                except Exception as e:
-                    f.write(f"{data} {function.__name__} error: {e}. Inputs: {args}, {kwargs}\n")
-                    return 0
+            data = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            try:
+                result = function(*args, **kwargs)
+                if filename:
+                    with open(filename, "a") as f:
+                        f.write(f"{data} {function.__name__} ok\n")
+                else:
+                    print(f"{data} {function.__name__} ok\n")
+                return result
+            except Exception as e:
+                if filename:
+                    with open(filename, "a") as f:
+                        f.write(f"{data} {function.__name__} error: {e}. Inputs: {args}, {kwargs}\n")
+                        return 0
+                else:
+                    print(f"{data} {function.__name__} error: {e}. Inputs: {args}, {kwargs}\n")
 
         return inner
 
     return my_decor
 
 
-@log(filename="src/mylog.txt")
-def my_function(x: int, y: int) -> int:
-    print(x, y)
-    return x + y
+# @log(filename="mylog.txt")
+# def my_function(x: Any, y: Any) -> Any:
+#     return x + y
